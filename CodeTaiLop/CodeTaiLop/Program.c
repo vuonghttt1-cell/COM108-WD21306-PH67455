637f33982f03749df6f9cae29bf3312a02a7ef28
Vuong Trinh <vuonghttt1@gmail.com>
2025-12-10 11:23:36 +0700

    commit all

--- CodeTaiLop/CodeTaiLop/Program.c
+++ CodeTaiLop/CodeTaiLop/Program.c
@@ -1,115 +1,83 @@
-<<<<<<< HEAD
-// CConsoleApplication.c : file nay chua 'main' function. 
-// Chuong trinh phan mem Bat dau thuc thi & Ket thuc o day.
-
 #include <stdio.h>
+void thongTinMonHoc() {
+	char tenMonHoc1[50], tenMonHoc2[50], tenMonHoc3[50];
+	int soTinChi1, soTinChi2, soTinChi3;
+	printf("Nhap ten mon hoc 1:");
+	scanf_s("%s", tenMonHoc1, sizeof(tenMonHoc1));
+	printf("Nhap so tin chi mon hoc 1:");
+	scanf_s("%d", &soTinChi1);
 
-int main()
-{
-    printf("Hi there!\n");
+	printf("Nhap ten mon hoc 2:");
+	scanf_s("%s", tenMonHoc2, sizeof(tenMonHoc2));
+	printf("Nhap so tin chi mon hoc 2:");
+	scanf_s("%d", &soTinChi2);
+
+	printf("Nhap ten mon hoc 3:");
+	scanf_s("%s", tenMonHoc3, sizeof(tenMonHoc3));
+	printf("Nhap so tin chi mon hoc 3:");
+	scanf_s("%d", &soTinChi3);
+
+	printf("Thong tin mon hoc:\n");
+	printf("Mon hoc 1: %s, So tin chi: %d\n", tenMonHoc1, soTinChi1);
+	printf("Mon hoc 2: %s, So tin chi: %d\n", tenMonHoc2, soTinChi2);
+	printf("Mon hoc 3: %s, so tin chi: %d\n", tenMonHoc3, soTinChi3);
+
 }
+void tinhTongSoLe() {
 
+	int n, tong = 0;
 
-// GV: AnhTT184
-=======
+	printf("Nhap n: ");
+	scanf_s("%d", &n);
 
-
-#include <stdio.h>
-int main() {
-	/*int main()
-	{
-		int chonChucNang;
-
-		{
-			printf("Menu:\n");
-			printf("1. Kiem tra so nguyen\n");
-			printf("2. Tim so chan le\n");
-			printf("3. Thoat\n");
-			printf("Chon chuc nang (1-3): ");
-			scanf_s("%d", &chonChucNang);
-			switch (chonChucNang)
-			{
-			case 1:
-			{
-
-				int so;
-				printf("Nhap mot so nguyen: ");
-				scanf_s("%d", &so);
-				if (so % 1 == 0)
-					printf("%d la so nguyen\n", so);
-				else
-					printf("%d khong phai la so nguyen\n", so);
-				break;
-
-			}
-			case 2:
-			{
-				int n;
-				printf("Nhap mot so: ");
-				scanf_s("%d", &n);
-
-				if (n % 2 == 0)
-					printf("%d la so chan\n", n);
-				else
-					printf("%d la so le\n", n);
-
-				return 0;
-			}
-
-
-
-
-
-
-			}
+	for (int i = 1; i <= n; i++) {
+		if (i % 2 != 0) {
+			tong += i;
 		}
-		}*/
-
-		/*int main() {
-			char name[50];
-
-			printf("Nhap ho ten: ");
-			fgets(name, sizeof(name), stdin);  // nh?p c? kho?ng tr?ng
-
-			printf("Ho ten cua ban: %s", name);
-
-			return 0;
-		}*/
-
-		/*int main() {
-			char ten[50];
-			printf("nhap vap ten cua ban:");
-			puts(ten);
-			gets(ten);
-			printf(" ten cua ban la %s", ten);
-			return 0;
-
-
-		}*/
-	void demMang2Chieu(); {
-		int array[2][3];
-		for (int i = 0; i < 2; i++) {
-			for (int j = 0; j < 3; j++) {
-				printf("Nhap cac phan tu trong mang array[%d][%d]: ", i, j);
-				scanf_s("%d", &array[i][j]);
-				
-
-			}
-
-		}
-		for (int i = 0; i < 2; i++) {
-			for (int j = 0; j < 3; j++) {
-				printf("%d,", array[i][j]);
-
-			}
-			printf("\n");
-		}
-
 
 	}
+	if (n % 2 == 0) {
+		printf("n la so chan\n");
+	}
+	else {
+		printf("n la so le\n");
+	}
+
+	printf("Tong cac so le tu 1 den %d la: %d\n", n, tong);
+
+
+
 }
 
+void thongTinDiemLab() {
 
+}
+int main() {
+	int luaChon;
+	char tiepTuc;
 
-		
->>>>>>> update code
+	do {
+		printf("1.Thong tin mon hoc\n");
+		printf("2.Tinh tong so le\n");
+		printf("3.Thong tin diem lab\n");
+		printf("4.Thoat\n");
+		printf("Nhap lua chon cua ban (1-4):");
+		scanf_s("%d", &luaChon);
+		switch (luaChon) {
+		case 1: thongTinMonHoc();
+			break;
+		case 2: tinhTongSoLe();
+			break;
+		case 3: thongTinDiemLab();
+			break;
+		case 4: printf("Thoat chuong trinh\n");
+			return 0;
+		default: printf("Lua chon khong hop le. Vui long chon lai.\n");
+
+		}
+		printf("Ban co muon tiep tuc khong? (y/n): ");
+		scanf_s(" %c", &tiepTuc, 1);
+
+	} while (tiepTuc == 'y' || tiepTuc == 'Y');
+	return 0;
+}
